cfa5cc6d6463e863f28ebbb73759949d066cfd66
Eric Constantinides <eric@flickerbox.com>
2015-07-09 02:50:26 -0700

    Add immediately invoked function expression to unclutter the global namespace

--- slide-toggle.js
+++ slide-toggle.js
@@ -2,14 +2,16 @@
 // Slide Toggle
 //------------------------------------------------------------------------------
 // need to add an optional container element that it needs to be activated.
-$('.js-slide-toggle--target').hide();
-$('.js-slide-toggle').off().on('click', function() {
-	if ($(this).hasClass('active')) {
-		$(this).removeClass('active');
-		$('.js-slide-toggle--target[data-target=' + $(this).attr('data-target') + ']').removeClass('active').slideUp(200);
-	} else {
-		$(this).addClass('active');
-		$('.js-slide-toggle--target[data-target=' + $(this).attr('data-target') + ']').addClass('active').slideDown(200);
-	}
-	event.preventDefault();
-});
+(function($) {
+	$('.js-slide-toggle--target').hide();
+	$('.js-slide-toggle').off().on('click', function() {
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$('.js-slide-toggle--target[data-target=' + $(this).attr('data-target') + ']').removeClass('active').slideUp(200);
+		} else {
+			$(this).addClass('active');
+			$('.js-slide-toggle--target[data-target=' + $(this).attr('data-target') + ']').addClass('active').slideDown(200);
+		}
+		event.preventDefault();
+	});
+})(jQuery);
